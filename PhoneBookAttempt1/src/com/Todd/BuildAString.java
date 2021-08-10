9129b5cee9f0a0af1c47437950a32b8681e660cf
todd6822 <40014780+todd6822@users.noreply.github.com>
2021-08-10 16:19:03 -0700

    Add files via upload

---
+++ PhoneBookAttempt1/src/com/Todd/BuildAString.java
@@ -0,0 +1,45 @@
+package com.Todd;
+
+import java.util.Scanner;
+
+public class BuildAString {
+
+	private String personString;
+
+	public String getPersonString() {
+		
+		return personString;
+		
+	}
+
+	public void setPersonString() {
+		
+		Scanner scanner=new Scanner(System.in);
+		//String inputString;
+	
+		
+		 System.out.println("please enter your first name");
+		 personString=scanner.nextLine()+ ", ";
+		 System.out.println("please enter your last name");
+		personString=personString+scanner.nextLine()+", ";
+		 System.out.println("please entern your adress");
+		 personString=personString+scanner.nextLine()+", ";
+		 System.out.println("please enter city");
+		 personString=personString+scanner.nextLine()+", ";
+		 System.out.println("please enter your state");
+		 personString=personString+scanner.nextLine()+", ";
+		 System.out.println("please enter your zip code");
+		 personString=personString+scanner.nextLine()+", ";
+		 System.out.println("please enter your telephone number");
+		 personString=personString+scanner.nextLine()+", ";
+		 
+		 this.personString=personString;
+		 
+		
+		
+	}
+	
+	
+	//canner.close();
+	
+}
